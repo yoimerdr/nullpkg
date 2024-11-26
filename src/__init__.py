0036ac33e21955c0e621b9ea1e4a45e5efaf8bd4
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2024-11-26 12:28:13 -0500

    <module>: added module init

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from . import utils, variables, objects, modules
+from .modules import File, Import, AliasImport, Package
+from .objects import Class, ClassMethod, Function
+from .variables import Variable, Parameter
+from .utils import paths
